370d320374676c82adde62d66d26826c5164a436
Tiago Araujo <tiar@nodesagency.com>
2019-08-30 11:01:20 +0200

    Added Ip Address Client

---
+++ src/Clients/IpAddressesClient.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace NStack\Clients;
+
+use NStack\Exceptions\FailedToParseException;
+use NStack\Models\IpAddress;
+
+/**
+ * Class IpAddressesClient
+ *
+ * @package NStack\Clients
+ * @author Tiago Araujo <tiar@nodesagency.com>
+ */
+class IpAddressesClient extends NStackClient
+{
+    /** @var string */
+    protected $path = 'geographic/ip-address';
+
+    /**
+     * index
+     *
+     * @return IpAddress
+     * @throws FailedToParseException
+     * @author Tiago Araujo <tiar@nodesagency.com>
+     */
+    public function index(): IpAddress
+    {
+        $response = $this->client->get($this->buildPath($this->path));
+        $contents = $response->getBody()->getContents();
+        $data = json_decode($contents, true);
+        return new IpAddress($data['data']);
+    }
+
+    /**
+     * show
+     *
+     *
+     * @param   String $ip
+     * @return  IpAddress
+     * @throws  FailedToParseException
+     * @author  Tiago Araujo <tiar@nodesagency.com>
+     */
+    public function show(String $ip): IpAddress
+    {
+        $response = $this->client->get($this->buildPath($this->path . '?ip=' . $ip));
+        $contents = $response->getBody()->getContents();
+        $data = json_decode($contents, true);
+        return new IpAddress($data['data']);
+    }
+}
