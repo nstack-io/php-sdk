cdbffc5fb3033bc697bb1fb416e66c9d31f3f227
Tiago Araujo <tiar@nodesagency.com>
2019-09-04 13:34:38 +0200

    Added validator client

---
+++ src/Models/EmailValidation.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace NStack\Models;
+
+/**
+ * Class EmailValidation
+ *
+ * @package NStack\Models
+ * @author Tiago Araujo <tiar@nodesagency.com>
+ */
+class EmailValidation extends Model
+{
+    /** @var boolean */
+    protected $ok;
+
+    /**
+     * parse
+     *
+     * @param array $data
+     * @author Tiago Araujo <tiar@nodesagency.com>
+     */
+    public function parse(array $data)
+    {
+        $this->ok = (int)$data['ok'];
+    }
+
+    /**
+     * toArray
+     *
+     * @return array
+     * @author Tiago Araujo <tiar@nodesagency.com>
+     */
+    public function toArray(): array
+    {
+        return ['ok' => $this->ok];
+    }
+
+    /**
+     * @return bool
+     */
+    public function isOk(): bool
+    {
+        return $this->ok;
+    }
+
+}
